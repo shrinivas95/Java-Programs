6f66600c5557de55cccbc245495a1d6f3eceb551
shrinivas95 <72912767+shrinivas95@users.noreply.github.com>
2020-10-19 23:44:32 +0530

    Add files via upload
    Program to Display Details of Teammates using Class and Object

---
+++ MemberDetails.java
@@ -0,0 +1,43 @@
+class MemberDetails{
+	public static void main(String[] args){
+		Details kavya= new Details();
+		kavya.name="Kavya";
+		kavya.place="Tarikere";
+		kavya.college="AIT";
+		kavya.hobbies="Drawing";
+		kavya.DOB="09/04/1998";
+		kavya.ListingDetails();
+
+		Details usha= new Details();
+		usha.name="Usha";
+		usha.place="Davanagere";
+		usha.college="AIT";
+		usha.hobbies="Playing";
+		usha.DOB="08/09/1998";
+		usha.ListingDetails();
+
+		Details tasmiya= new Details();
+		tasmiya.name= "Tasmiya";
+		tasmiya.place= "Chikkamagaluru";
+		tasmiya.college= "AIT";
+		tasmiya.hobbies= "Reading";
+		tasmiya.DOB= "10/01/1998";
+		tasmiya.ListingDetails();
+
+		Details ramya= new Details();
+		ramya.name="Ramya";
+		ramya.place="Chikkamagaluru";
+		ramya.college="AIT";
+		ramya.hobbies="Drawing";
+		ramya.DOB="24/08/1998";
+		ramya.ListingDetails();
+
+		Details shrinivas= new Details();
+		shrinivas.name="Shrinivas";
+		shrinivas.place="Jamkhandi";
+		shrinivas.college="BGMIT";
+		shrinivas.hobbies="Cricket";
+		shrinivas.DOB="21/02/1995";
+		shrinivas.ListingDetails();
+}
+}
