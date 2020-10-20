20122c807e8953ed37bf39cbd0966c65f5cf3625
shrinivas95 <72912767+shrinivas95@users.noreply.github.com>
2020-10-20 08:07:42 +0530

    Add files via upload
    Example of using class and object

---
+++ FlowerDemo.java
@@ -0,0 +1,16 @@
+class FlowerDemo{
+
+public static void main(String args[]){
+
+Flower rose = new Flower();
+rose.name="Rose";
+rose.color="Red";
+
+Flower jasmine = new Flower();
+jasmine.name="Jasmine";
+jasmine.color="White";
+
+rose.blooming();
+jasmine.blooming();
+}
+}
